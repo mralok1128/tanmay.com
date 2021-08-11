7aeecd1b85e86c567d0b26fdaeb40fe50906f63d
mralok1128 <88083766+mralok1128@users.noreply.github.com>
2021-08-11 12:18:41 +0530

    '
    \

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$name=$_POST['name'];
+$email=$_POST['email'];
+$message=$_POST['message'];
+$to = "mralok1128@gmail.com";
+$subject = "mralok1128@gmail.com from https://mralok1128.github.io/tanmay.github.io";
+$
